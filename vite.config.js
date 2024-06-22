416e3053784e2ac6aa818fc7c7a19d3275fa4e7a
sbsatya82 <satyabratamahanta82@gmail.com>
2024-06-22 10:16:08 +0530

    add backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api/v1': 'http://localhost:8000'
+      '/api/v1': 'https://ecommerce-backend-r1lo.onrender.com'
       }
   },
   plugins: [react()],
